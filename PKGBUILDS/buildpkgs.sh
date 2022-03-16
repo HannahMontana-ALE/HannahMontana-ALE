ba3c9bc63b8e766f84c43bc7d104f524a0db721c
Michael Monaco <ios8jailbreakpangu@gmail.com>
2022-03-15 22:20:30 -0400

    Rebuild after implemented fixes

--- PKGBUILDS/buildpkgs.sh
+++ PKGBUILDS/buildpkgs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rm ./folders.txt
+rm -r ../repo
+mkdir ../repo
 find "$(cd .; pwd)" -maxdepth 1 -type d > folders.txt
 
 mapfile -t myArray < ./folders.txt
@@ -10,5 +12,11 @@ done < ./folders.txt
 cd ../
 cd ../
 mv ./PKGBUILDS/*.pkg.tar.zst ./repo/
+rm ./PKGBUILDS/folders.txt
+cd ./repo
 
-rm ./folders.txt
+repo-add hannahmontana-arch.db.tar.zst *.pkg.tar.zst
+rm ./hannahmontana-arch.db
+rm ./hannahmontana-arch.files
+cp hannahmontana-arch.db.tar.zst hannahmontana-arch.db
+cp hannahmontana-arch.files.tar.zst hannahmontana-arch.files
