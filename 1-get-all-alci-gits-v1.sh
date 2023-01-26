a5b80c7d136d580c445857dc885bd10e02a75a6c
ThePoorPilot <78394939+ThePoorPilot@users.noreply.github.com>
2023-01-26 13:38:13 +0000

    Squashed 'isobuild/' changes from 42e1993..cb6539e
    cb6539e Merge pull request #4 from arch-linux-calamares-installer/master
    5692b87 archiso 69
    36dc0fb archiso
    4f8b57b up
    49a710a archiso
    2e21fba archiso
    dafde00 from xanmod-edge to xanmod
    e9f5cf9 archiso 68
    5003566 script 9
    22d62c7 archiso 68
    68367ec test push
    4a3ec4b archiso 67
    
    git-subtree-dir: isobuild
    git-subtree-split: cb6539e22b2bed0f2acc6a6a7b365cde9a5fb3ed

--- 1-get-all-alci-gits-v1.sh
+++ 1-get-all-alci-gits-v1.sh
@@ -18,11 +18,12 @@ echo "Fill the array with the original folders first"
 
 directories=(
 alci-calamares-config/
+alci-calamares-config-btrfs/
 alci-calamares-config-dev/
 alci-calamares-config-hardened/
 alci-calamares-config-lts/
 alci-calamares-config-pure/
-alci-calamares-config-xanmod-edge/
+alci-calamares-config-xanmod/
 alci-calamares-config-zen/
 alci-dwm/
 alci-dwm-nemesis/
@@ -31,7 +32,7 @@ alci-iso-dev/
 alci-iso-hardened/
 alci-iso-lts/
 alci-iso-pure/
-alci-iso-xanmod-edge/
+alci-iso-xanmod/
 alci-iso-zen/
 alci-pkgbuild/
 alci_repo/
